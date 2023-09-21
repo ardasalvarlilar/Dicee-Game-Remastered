90ac0a16a56012e0870686db6e392c763d77424e
Arda Şalvarlılar <ardasalvarlilar@192.168.1.6>
2023-09-21 19:45:45 +0300

    competed all the functions

--- script.js
+++ script.js
@@ -14,14 +14,29 @@ const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.classList.add('hidden')
+let scores 
+let currentScore 
+let activePlayer 
+let playing 
 
-const scores = [0, 0]
-let currentScore = 0
-let activePlayer = 0
-let playing = true
+const init = () => {
+  scores = [0, 0]
+  currentScore = 0
+  activePlayer = 0
+  playing = true
+
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = 0
+  current1El.textContent = 0
+  
+  diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+}
+init()
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0
@@ -66,7 +81,7 @@ btnHold.addEventListener('click', () => {
     scores[activePlayer] += currentScore // scores[1] = scores[1] + currentScore
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
   
-    if(scores[activePlayer] >= 20){
+    if(scores[activePlayer] >= 100){
       playing = false
       diceEl.classList.add('hidden')
       document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
@@ -76,3 +91,6 @@ btnHold.addEventListener('click', () => {
     }
   }
 })
+
+
+btnNew.addEventListener('click', init())
